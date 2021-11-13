29a75b72d3407dbfac6833db115e7ffc0819c808
jbtule <jay.tuley@ekonbenefits.com>
2021-11-12 23:01:01 -0600

    initial port not quite working

---
+++ Tests/Tests.fs
@@ -0,0 +1,159 @@
+﻿module Tests
+
+open System
+open Xunit
+open TitleCaseSharp
+
+[<Theory>]
+[<InlineData("",
+    "")>]
+[<InlineData("word/word",
+    "Word/Word")>]
+
+[<InlineData("a title and/or string",
+       "A Title and/or String")>]
+[<InlineData("dance with me/let’s face the music and dance",
+"Dance With Me/Let’s Face the Music and Dance")>]
+[<InlineData("a-b end-to-end two-not-three/three-by-four/five-and",
+"A-B End-to-End Two-Not-Three/Three-by-Four/Five-And")>]
+[<InlineData("34th 3rd 2nd",
+       "34th 3rd 2nd")>]
+[<InlineData("Q&A with steve jobs: 'that's what happens in technology'",
+      "Q&A With Steve Jobs: 'That's What Happens in Technology'")>]
+[<InlineData("What is AT&T's problem?",
+    "What Is AT&T's Problem?")>]
+[<InlineData("Apple deal with AT&T falls through",
+   "Apple Deal With AT&T Falls Through")>]
+[<InlineData("Words with all consonants like cnn are acronyms",
+"Words With All Consonants Like CNN Are Acronyms")>]
+[<InlineData("this v that",
+  "This v That")>]
+[<InlineData("this v. that",
+    "This v. That")>]
+[<InlineData("this vs that",
+"This vs That")>]
+[<InlineData("this vs. that",
+ "This vs. That")>]
+[<InlineData("The SEC's Apple probe: what you need to know",
+      "The SEC's Apple Probe: What You Need to Know")>]
+[<InlineData("'by the Way, small word at the start but within quotes.'",
+     "'By the Way, Small Word at the Start but Within Quotes.'")>]
+[<InlineData("Small word at end is nothing to be afraid of",
+    "Small Word at End Is Nothing to Be Afraid Of")>]
+[<InlineData("Starting Sub-Phrase With a Small Word: a Trick, Perhaps?",
+"Starting Sub-Phrase With a Small Word: A Trick, Perhaps?")>]
+[<InlineData("Sub-Phrase With a Small Word in Quotes: 'a Trick, Perhaps?'",
+"Sub-Phrase With a Small Word in Quotes: 'A Trick, Perhaps?'")>]
+[<InlineData("sub-phrase with a small word in quotes: \"a trick, perhaps?\"",
+  "Sub-Phrase With a Small Word in Quotes: \"A Trick, Perhaps?\"")>]
+[<InlineData("Starting a Hyphen Delimited Sub-Phrase With a Small Word - a Trick, Perhaps?",
+    "Starting a Hyphen Delimited Sub-Phrase With a Small Word - A Trick, Perhaps?")>]
+[<InlineData("Starting a Hyphen Delimited Sub-Phrase With a Small Word - a Trick, Perhaps?",
+    "Starting a Hyphen Delimited Sub-Phrase With a Small Word - A Trick, Perhaps?")>]
+[<InlineData("Hyphen Delimited sub-phrase with a small word in quotes - \"a trick, perhaps?\"",
+"Hyphen Delimited Sub-Phrase With a Small Word in Quotes - \"A Trick, Perhaps?\"")>]
+[<InlineData("Snakes on a Plane - The TV Edit - The Famous Line",
+"Snakes on a Plane - The TV Edit - The Famous Line")>]
+[<InlineData("Starting an Em Dash Delimited Sub-Phrase With a Small Word — a Trick, Perhaps?",
+"Starting an Em Dash Delimited Sub-Phrase With a Small Word — A Trick, Perhaps?")>]
+[<InlineData("Em Dash Delimited Sub-Phrase With a Small Word in Quotes — 'a Trick, Perhaps?'",
+ "Em Dash Delimited Sub-Phrase With a Small Word in Quotes — 'A Trick, Perhaps?'")>]
+[<InlineData("Em Dash Delimited sub-phrase with a small word in quotes — \"a trick, perhaps?\"",
+     "Em Dash Delimited Sub-Phrase With a Small Word in Quotes — \"A Trick, Perhaps?\"")>]
+[<InlineData("Snakes on a Plane — The TV Edit — The Famous Line",
+       "Snakes on a Plane — The TV Edit — The Famous Line")>]
+[<InlineData("EPISODE 7 — THE FORCE AWAKENS",
+"Episode 7 — The Force Awakens")>]
+[<InlineData("episode 7 – The force awakens",
+"Episode 7 – The Force Awakens")>]
+[<InlineData("THE CASE OF X ≤ 7",
+"The Case of X ≤ 7")>]
+[<InlineData("the case of X ≤ 7",
+      "The Case of X ≤ 7")>]
+[<InlineData("\"Nothing to Be Afraid of?\"",
+   "\"Nothing to Be Afraid Of?\"")>]
+[<InlineData("\"Nothing to be Afraid Of?\"",
+"\"Nothing to Be Afraid Of?\"")>]
+[<InlineData("a thing",
+       "A Thing")>]
+[<InlineData("2lmc Spool: 'gruber on OmniFocus and vapo(u)rware'",
+   "2lmc Spool: 'Gruber on OmniFocus and Vapo(u)rware'")>]
+[<InlineData("this is just an example.com",
+"This Is Just an example.com")>]
+[<InlineData("this is something listed on del.icio.us",
+"This Is Something Listed on del.icio.us")>]
+[<InlineData("iTunes should be unmolested",
+    "iTunes Should Be Unmolested")>]
+[<InlineData("reading between the lines of steve jobs’s ‘thoughts on music’",
+   "Reading Between the Lines of Steve Jobs’s ‘Thoughts on Music’")>]
+[<InlineData("seriously, ‘repair permissions’ is voodoo",
+     "Seriously, ‘Repair Permissions’ Is Voodoo")>]
+[<InlineData("generalissimo francisco franco: still dead; kieren McCarthy: still a jackass",
+"Generalissimo Francisco Franco: Still Dead; Kieren McCarthy: Still a Jackass")>]
+[<InlineData("O'Reilly should be untouched",
+      "O'Reilly Should Be Untouched")>]
+[<InlineData("my name is o'reilly",
+  "My Name Is O'Reilly")>]
+[<InlineData("WASHINGTON, D.C. SHOULD BE FIXED BUT MIGHT BE A PROBLEM",
+ "Washington, D.C. Should Be Fixed but Might Be a Problem")>]
+[<InlineData("THIS IS ALL CAPS AND SHOULD BE ADDRESSED",
+"This Is All Caps and Should Be Addressed")>]
+[<InlineData("Mr McTavish went to MacDonalds",
+"Mr McTavish Went to MacDonalds")>]
+[<InlineData("this shouldn't\nget mangled",
+"This Shouldn't\nGet Mangled")>]
+[<InlineData("this is http://foo.com",
+ "This Is http://foo.com")>]
+[<InlineData("mac mc MAC MC machine",
+      "Mac Mc MAC MC Machine")>]
+[<InlineData("FOO BAR 5TH ST",
+       "Foo Bar 5th St")>]
+[<InlineData("foo bar 5th st",
+    "Foo Bar 5th St")>]
+[<InlineData("l'grange l'grange l'Grange l'Grange",
+ "l'Grange l'Grange l'Grange l'Grange")>]
+[<InlineData("L'grange L'grange L'Grange L'Grange",
+      "l'Grange l'Grange l'Grange l'Grange")>]
+[<InlineData("l'GranGe",
+   "l'GranGe")>]
+[<InlineData("o'grange O'grange o'Grange O'Grange",
+   "O'Grange O'Grange O'Grange O'Grange")>]
+[<InlineData("o'grange's O'grange's o'Grange's O'Grange's",
+    "O'Grange's O'Grange's O'Grange's O'Grange's")>]
+[<InlineData("O'GranGe",
+     "O'GranGe")>]
+[<InlineData("o'melveny/o'doyle o'Melveny/o'doyle O'melveny/o'doyle o'melveny/o'Doyle o'melveny/O'doyle",
+"O'Melveny/O'Doyle O'Melveny/O'Doyle O'Melveny/O'Doyle O'Melveny/O'Doyle O'Melveny/O'Doyle")>]
+// These 'Mc' cases aim to ensure more consistent/predictable behavior.
+// The examples here are somewhat contrived, and are subject to change
+// if there is a compelling argument for updating their behavior.
+// See https://github.com/ppannuto/python-titlecase/issues/64
+[<InlineData("mccay-mcbut-mcdo mcdonalds/mcby",
+       "McCay-McBut-McDo McDonalds/McBy")>]
+[<InlineData("oblon, spivak, mcclelland, maier & neustadt",
+     "Oblon, Spivak, McClelland, Maier & Neustadt")>]
+[<InlineData("Mcoblon, spivak, mcclelland, mcmaier, & mcneustadt",
+ "McOblon, Spivak, McClelland, McMaier, & McNeustadt")>]
+[<InlineData("mcfoo-bar, MCFOO-BAR, McFoo-bar, McFoo-Bar, mcfoo-mcbar, foo-mcbar",
+"McFoo-Bar, McFoo-Bar, McFoo-Bar, McFoo-Bar, McFoo-McBar, Foo-McBar")>]
+[<InlineData("'QUOTE' A GREAT",
+    "'Quote' a Great")>]
+[<InlineData("‘QUOTE’ A GREAT",
+      "‘Quote’ a Great")>]
+[<InlineData("“YOUNG AND RESTLESS”",
+"“Young and Restless”")>]
+[<InlineData("EL NIÑO A ARRIVÉ HIER",
+   "El Niño a Arrivé Hier")>]
+[<InlineData("YEA NO",
+       "Yea No")>]
+[<InlineData("ÝÆ ÑØ",
+   "Ýæ Ñø")>]
+[<InlineData("yea no",
+     "Yea No")>]
+[<InlineData( "ýæ ñø",
+"Ýæ Ñø")>]
+//https://github.com/ppannuto/python-titlecase/pull/67
+[<InlineData("Mr mr Mrs Ms Mss Dr dr , Mr. and Mrs. Person",
+"Mr Mr Mrs Ms MSS Dr Dr , Mr. And Mrs. Person")>]
+let ``Generated Tests`` (input:string,  expected:string) =
+    Assert.Equal(expected, input |> TitleCase.transform )
---
+++ TitleCaseSharp/Library.fs
@@ -0,0 +1,164 @@
+﻿namespace TitleCaseSharp
+
+open System.Text.RegularExpressions
+open System.Globalization
+open System
+
+(*
+Original Perl version by: John Gruber http://daringfireball.net/ 10 May 2008
+Python version by Stuart Colville http://muffinresearch.co.uk
+F# version ported by James Tuley (Ekon Benefits) from python https://github.com/ppannuto/python-titlecase 2de89b2  
+License: http://www.opensource.org/licenses/mit-license.php
+*)
+
+module TitleCase =
+
+    let SMALL = @"a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?"
+    let PUNCT = """!"“#$%&'‘()*+,\-–‒—―./:;?@[\\\]_`{|}~"""
+    let SMALL_WORDS = Regex($@"^(%s{SMALL})$", RegexOptions.IgnoreCase ||| RegexOptions.Compiled)
+    let SMALL_FIRST = Regex($@"^([%s{PUNCT}]*)(%s{SMALL})\b", RegexOptions.IgnoreCase ||| RegexOptions.Compiled)
+    let SMALL_LAST = Regex($@"\b(%s{SMALL})[%s{PUNCT}]?$", RegexOptions.IgnoreCase ||| RegexOptions.Compiled)
+    let SUBPHRASE = Regex($@"([:.;?!\-–‒—―][ ])(%s{SMALL})", RegexOptions.Compiled)
+    let MAC_MC = Regex(@"^([Mm]c|MC)(\w.+)", RegexOptions.Compiled)
+    let MR_MRS_MS_DR = Regex(@"^((m((rs?)|s))|Dr)$", RegexOptions.IgnoreCase ||| RegexOptions.Compiled)
+    let INLINE_PERIOD = Regex(@"[\w][.][\w]", RegexOptions.IgnoreCase ||| RegexOptions.Multiline ||| RegexOptions.Compiled)
+    let UC_ELSEWHERE = Regex($@"[%s{PUNCT}]*?[a-zA-Z]+[A-Z]+?", RegexOptions.Compiled)
+    let CAPFIRST = Regex($@"^[%s{PUNCT}]*?([\w])", RegexOptions.Compiled)
+    let APOS_SECOND = Regex(@"^[dol]['‘][\w]+(?:['s]{2})?$", RegexOptions.IgnoreCase ||| RegexOptions.Compiled)
+    let UC_INITIALS = Regex(@"^(?:[A-Z]\.|[A-Z]\.[A-Z])+$")
+    let CONSONANTS = Regex($@"\A[%s{Set.difference (Set ['a'..'z']) (Set ['a';'e';'i';'o';'u';'y']) |> string}]+\Z",
+                            RegexOptions.IgnoreCase ||| RegexOptions.Multiline ||| RegexOptions.Compiled)
+
+    type PreProcessed = Mutable of string | Immutable of string
+
+    [<AutoOpen>]
+    module Patterns =
+        let Preserve = Immutable >> Some
+        let Accept = Mutable >> Some
+        let capitalize = CultureInfo.CurrentCulture.TextInfo.ToTitleCase
+
+        let (|Callback|_|) callback all_caps word =
+            match callback with
+            | Some(cb) -> word |> cb all_caps |> Preserve
+            | _ -> None;
+
+        let (|AllCaps|_|) word =
+            if UC_INITIALS.IsMatch(word) then
+                word |> Accept
+            else
+                None
+
+        let (|AposSecond|_|) word =
+            match APOS_SECOND.IsMatch(word) with
+            | true -> 
+                let casing =
+                    if "aeiouAEIOU".Contains(string word[0]) then
+                        Char.ToLower
+                    else
+                        Char.ToUpper
+                seq { casing(word[0]); word[1]; Char.ToUpper(word[2]); yield! word[3..] } 
+                |> string 
+                |> Accept
+            | false  -> None
+                    
+        let (|MacMc|_|) transf callback word =
+            match MAC_MC.Match(word) with
+            | m when m.Success ->
+                $"%s{capitalize(m.Groups[1].Value)}%s{m.Groups[2].Value |> transf callback true}" |> Accept
+            | _ -> None
+                    
+        let (|MrMrsMsDr|_|) word =
+            match MR_MRS_MS_DR.IsMatch(word) with
+            | true -> 
+                seq { Char.ToUpper(word[0]); yield! word[1..]}
+                |> string 
+                |> Accept
+            | false -> None
+
+        let (|InlinePeriod|_|) all_caps word =
+            if INLINE_PERIOD.IsMatch(word) || (not all_caps && UC_ELSEWHERE.IsMatch(word)) then
+                word |> Accept
+            else
+                None
+    
+        let (|SmallWords|_|) word =
+            if SMALL_WORDS.IsMatch(word) then
+                word.ToLower() |> Accept
+            else
+                None
+
+        let (|Slashes|_|) transf callback (word:string) =
+            if word.Contains("/") &&  not <| word.Contains("//") then
+                word.Split('/') 
+                |> Seq.map(fun t -> t |> transf callback false ) 
+                |> String.concat "/" |> Accept
+            else
+                None
+
+        let (|Hyphens|_|) transf callback (word:string) =
+            if word.Contains("-") then
+                word.Split('-') 
+                |> Seq.map(fun t -> t |> transf callback false ) 
+                |> String.concat "-" |> Accept
+            else
+                None
+
+        let (|Abbreviation|_|) all_caps (word:string) =
+            let word' = if all_caps then word.ToLower() else word
+            if CONSONANTS.IsMatch(word') && word.Length > 2 then
+                word.ToUpper() |> Accept
+            else
+                None
+  
+    [<CompiledName("TransformWithCallback")>]
+    let transformWithCallback (callback: Func<string, bool, string>) (text:string) =
+        let rec transf callback small_first_last text  = 
+            let processed = 
+                seq {
+                    let lines = Regex.Split(text, @"[\r\n]+");
+                    for line in lines do
+                        let all_caps = line.ToUpperInvariant() = line
+                        let words = Regex.Split(line, @"[\t ]");
+                        let tc_line =
+                            [|
+                                for word in words do
+                                   match word with
+                                   | Callback callback all_caps c -> c
+                                   | AllCaps a -> a
+                                   | AposSecond p -> p
+                                   | MacMc transf callback mc -> mc
+                                   | MrMrsMsDr mr -> mr
+                                   | InlinePeriod all_caps ip -> ip
+                                   | SmallWords sw -> sw
+                                   | Slashes transf callback sl -> sl
+                                   | Hyphens transf callback h -> h
+                                   | Abbreviation all_caps ab -> ab
+                                   | w -> CAPFIRST.Replace(w,fun m->m.Groups[0].Value.ToUpper()) |> Mutable
+                            |]
+                        let tc_line' =
+                            if small_first_last then
+                                seq {
+                                    let last = tc_line.Length-1
+                                    match tc_line[0] with 
+                                    | Mutable s -> SMALL_FIRST.Replace(s, fun m -> $"%s{m.Groups[1].Value}%s{capitalize(m.Groups[2].Value)}") |> Mutable
+                                    | x -> x
+                                    yield! tc_line[1..last-1]
+                                    match tc_line[last] with 
+                                    | Mutable s -> SMALL_LAST.Replace(s, fun m -> capitalize(m.Groups[0].Value)) |> Mutable
+                                    | x -> x
+                            
+                                }
+                            else
+                                tc_line
+                        let result = tc_line' |> Seq.map (function | Mutable m -> m | Immutable i -> i) |> String.concat " "
+                        SUBPHRASE.Replace(result, fun m -> capitalize(m.Groups[0].Value))
+                }
+            processed |> String.concat Environment.NewLine
+            
+        let callback' = 
+            callback 
+            |> Option.ofObj
+            |> Option.map (fun f'-> (fun x y-> f'.Invoke(y, x)))
+        text |> transf callback' true
+    [<CompiledName("Transform")>]
+    let transform = transformWithCallback null
